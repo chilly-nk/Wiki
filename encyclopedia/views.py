d1d26350461c83814a9400ea8550b650fc4ae7b4
$username <$narek.ch@gmail.com>
2024-07-08 20:38:50 +0400

    Etry Page. 1. Added a template entry.html, which is rendered when /wiki/TITLE is accessed 2. Used library markdown in order to render the markdown content into html. 3. Applied a filter |safe in order to convince Django not to escape the contents, which were rendered in views.entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
 from . import util
+import markdown
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    contents = markdown.markdown(util.get_entry(title))
+    return render(request, "encyclopedia/entry.html", {
+        'title': title,
+        'contents': contents,
     })
 
