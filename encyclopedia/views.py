8567200319a40bd98f76c591fe7955bd47e6151d
$username <$narek.ch@gmail.com>
2024-07-10 20:27:06 +0400

    Entry page requirement fulfilled with all points

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,7 +10,12 @@ def index(request):
     })
 
 def entry(request, title):
-    contents = markdown.markdown(util.get_entry(title))
+    contents_raw = util.get_entry(title)
+    if contents_raw == None:
+        contents_raw = f"#Error: '{title}' not found\n\n From Wiki, chilly-nk's encyclopedia.\n\n Wiki does not have an article with this exact name. Please visit [Home](/wiki) to see all available articles."
+        contents = contents = markdown.markdown(contents_raw)
+    else:
+        contents = markdown.markdown(contents_raw)
     return render(request, "encyclopedia/entry.html", {
         'title': title,
         'contents': contents,
