7cc0bb91e1b71647ce241ff4ecfbfb31234437e8
chilly-nk <narek.ch@gmail.com>
2025-03-25 23:42:13 +0400

    Search ready

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import redirect
 # from django.urls import reverse
 
 class SearchForm(forms.Form):
-    query_term = forms.CharField(label='Search Nareklopedia', max_length=100) # this attribute must be the same as name='query_term' in html's <input name='query_term'> etc.
+    query = forms.CharField(label='Search Nareklopedia', max_length=100) # this attribute must be the same as name='query' in html's <input name='query'> etc.
 
 def index(request):
     entries = util.list_entries()
@@ -31,12 +31,23 @@ def entry(request, title):
 def search(request):
     form = SearchForm(request.GET)
     if form.is_valid():
-        query_term = form.cleaned_data['query_term']
-        print(f'Search term: {query_term}')
-        # return redirect('encyclopedia:entry', title=query_term)
-        return entry(request, query_term)
+        query = form.cleaned_data['query'].lower()
+        entries = util.list_entries()
+        entries_lower = [entry.lower() for entry in entries]    
+        if query in entries_lower:
+            return entry(request, query)
+        else:
+            partial_match = [entry for entry, entry_lower in zip(entries, entries_lower) if query in entry_lower]
+            if len(partial_match) > 0:
+                message = f"Sorry, we couldn't find an exact match for '{query}', but here are some similar items:"
+            else:
+                message = f"Sorry, we couldn't find any match for '{query}'."
+            return render(request, 'encyclopedia/search_results.html', {
+                'message': message,
+                'partial_match': partial_match,
+            })
     
-    return index(request)
+    # return index(request)
 
     
 
